df02b7316d6d9a41013f89203ae606a135a3fa2b
litala123 <litala@rpi.edu>
2021-06-09 01:32:18 -0400

    Parses for source and output files from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use std::env;
+use std::process;
+
+enum ArgParseStates {
+    General,
+    OutputFile,
+}
+
 fn main() {
-    println!("Hello, Kiwi dev!");
+    println!("Kiwi Compiler\n");
+
+    let args: Vec<String> = env::args().collect();
+    let mut out_file = "build.kasm";
+
+    if args.len() == 1 {
+        eprintln!("Error: not enough arguments.");
+        process::exit(1);
+    }
+
+    let mut arg_parse_state = ArgParseStates::General;
+    let mut sources = vec![];
+
+    for arg in &args[1..args.len()] {
+        match arg_parse_state {
+            ArgParseStates::General => {
+                // println!("General State {:?}", arg);
+
+                match arg.as_str() {
+                    "-o" => {
+                        arg_parse_state = ArgParseStates::OutputFile;
+                        // println!("\tMoving to the OutputFile State\n");
+                        continue;
+                    },
+                    _ => {
+                        sources.push(arg);
+                        // println!("\tAdded \"{}\" to source list\n", arg);
+                        continue;
+                    }
+                }
+            },
+            ArgParseStates::OutputFile => {
+                // println!("OutputFile State {:?}", arg);
+                out_file = arg;
+                arg_parse_state = ArgParseStates::General;
+                // println!("\tMoving to the General State\n");
+                continue;
+            },
+        }
+    }
+    
+    print!("Source files:\n");
+    for s in sources {
+        print!("\t{}\n", s);
+    }
+
+    println!("\nOutput file:\n\t{}", out_file);
 }
