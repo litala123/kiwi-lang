3ae7693e5021455ce7173d6a9741bbd6a3b7dc39
litala123 <litala@rpi.edu>
2021-10-06 23:16:07 -0400

    Switched from LALRPOP to pest, started parser and code gen

--- src/tests.rs
+++ src/tests.rs
@@ -1,25 +1,11 @@
-use super::*;
+// use super::*;
 
 #[test]
-pub fn i32_literal_parser() {
-    assert!(parser::I32LiteralParser::new().parse("1").is_ok());
-    assert!(parser::I32LiteralParser::new().parse("1234567890").is_ok());
-    assert!(parser::I32LiteralParser::new().parse("x").is_err());
-    assert!(parser::I32LiteralParser::new().parse("123i").is_ok());
-    assert!(parser::I32LiteralParser::new().parse("123u").is_err());
-    assert!(parser::I32LiteralParser::new().parse("123x").is_err());
-    assert!(parser::I32LiteralParser::new().parse("123uu").is_err());
-    assert!(parser::I32LiteralParser::new().parse("12.3").is_err());
+pub fn _import() {
+    assert!(true);
 }
 
 #[test]
-pub fn u32_literal_parser() {
-    assert!(parser::U32LiteralParser::new().parse("1").is_err());
-    assert!(parser::U32LiteralParser::new().parse("12345678901234567890").is_err());
-    assert!(parser::U32LiteralParser::new().parse("x").is_err());
-    assert!(parser::U32LiteralParser::new().parse("123i").is_err());
-    assert!(parser::U32LiteralParser::new().parse("123u").is_ok());
-    assert!(parser::U32LiteralParser::new().parse("123x").is_err());
-    assert!(parser::U32LiteralParser::new().parse("123uu").is_err());
-    assert!(parser::U32LiteralParser::new().parse("12.3").is_err());
+pub fn _func() {
+    assert!(true);
 }
